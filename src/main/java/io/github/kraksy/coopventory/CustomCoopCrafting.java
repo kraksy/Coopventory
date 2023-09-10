37093c080cbc008b4ae9a4268b58fe45261ccbdd
kraksy <kraksy8@gmail.com>
2023-09-10 13:51:23 +0200

    yes

--- src/main/java/io/github/kraksy/coopventory/CustomCoopCrafting.java
+++ src/main/java/io/github/kraksy/coopventory/CustomCoopCrafting.java
@@ -1,25 +1,18 @@
 package io.github.kraksy.coopventory;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.logging.Logger;
 
 import static org.bukkit.Bukkit.getLogger;
@@ -28,8 +21,13 @@ public class CustomCoopCrafting implements Listener {
     @EventHandler
     public void getItemInCrafting(PrepareItemCraftEvent event){
         CraftingInventory craftingInventory = event.getInventory();
+        List<Player> playerList = new ArrayList<>();
+        playerList.add((Player) event.getViewers());
 
-        // 2D array for the crafting layout , stolen from chatgpt
+        sendCraftingInventory();
+    }
+
+    public void getSlot(CraftingInventory craftingInventory){
         ItemStack[][] matrix = new ItemStack[3][3];
 
         for (int row = 0; row < 3; row++){
@@ -40,50 +38,20 @@ public class CustomCoopCrafting implements Listener {
             }
         }
 
-        List<Player> playerList = new ArrayList<>();
-        List<ItemStack[]> itemStackList = new ArrayList<>();
-
-        itemStackList.add(items);
-        playerList.add((Player) event.getViewers());
-
-        //todo maybe make a for loop where it will look at every slot and it will make a array with where are the items
-
-
-
-
-        mergeItemStack(itemStackList, craftingInventory);
-        saveConfig(playerList, craftingInventory);
     }
 
-    public void mergeItemStack(List<ItemStack[]> itemStacks, CraftingInventory craftingInventory){
-        for (ItemStack[] items : itemStacks){
-            for (ItemStack itemStack : items){
-                //todo set specific item to its place
-                craftingInventory.setItem(); // set item in its place
-            }
-        }
-
-        //todo send the geral crafting table to all the players
-    }
-
-    public void saveConfig(List<Player> playerList, CraftingInventory craftingInventory){
-
+    public void sendCraftingInventory(List<Player> playerList, CraftingInventory craftingInventory){
         Logger logger = getLogger();
 
-        // for every player send merged list of items , send it like crafting inventory
         for (HumanEntity player : playerList){
             if (player != null){
-                itemStacks.get(i);
+
+                getSlot(craftingInventory);
+
+                player.sendMessage();
 
             }
         }
-
-    }
-
-    public void sendPlayer(Player player, CraftingInventory craftingInventory, List<ItemStack[]> itemStacks){
-
-        craftingInventory.setMatrix();
-
     }
 
     @EventHandler
@@ -91,14 +59,6 @@ public class CustomCoopCrafting implements Listener {
 
         Player player = (Player) event.getWhoClicked();
         Event.@NotNull Result item = event.getResult();
-
-        // 4 rows today B ]
-
-
-
-        //todo send players all items in crafting table and sync
-        //todo it should be done after that T T
-        //todo maybe im gonna need LAST event for finishing the crafting
 
     }
 }
